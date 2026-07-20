4ac4f4d077da3d3441f3852326de48af25e62e55
Waseque Siddiqui <wasequesiddiqui@hotmail.com>
2026-07-20 19:12:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <p>Welcome to my first React site 🚀</p>
+    </div>
+  )
+}
+
+export default App
